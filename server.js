4431f7c67a5bc07947eb7996c12b2d2b99db6e6e
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-02 08:38:19 +0000

    Added the routing middleware and error handling middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const { dbConnect } = require("./db/config.js")
+const notesRoute = require("./route/notes.js")
 const PORT = process.env.PORT
+
 
 
 // calling the function to connect to the database
@@ -9,10 +11,25 @@ dbConnect()
 
 const app = express()
 
+// MIDDLEWARES
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("My Notes")
+// ROUTES
+app.use("/", notesRoute)
+
+
+// ERROR HANDLING MIDDLEWARE
+app.use((err,req,res,next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(500).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
