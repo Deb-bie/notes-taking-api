aafb1b7f3aefb91788b431ab26246ab8f79b8db4
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-02 08:18:59 +0000

    Called the database connection function in the server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { dbConnect } = require("./db/config.js")
 const PORT = process.env.PORT
 
+
+// calling the function to connect to the database
+dbConnect()
+
 const app = express()
+
 
 app.get("/", (req, res) => {
     res.send("My Notes")
